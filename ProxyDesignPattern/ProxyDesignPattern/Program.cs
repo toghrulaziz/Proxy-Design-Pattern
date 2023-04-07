5d9527c947c8fe6cda09effe30e3bd2774f4594a
toghrulaziz <togrul1609@gmail.com>
2023-04-07 16:40:50 +0400

    Proxy Design Pattern

---
+++ ProxyDesignPattern/ProxyDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ProxyDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPayment payment = new ProxyPayment("12345");
+
+            bool result = payment.Pay(100.50M);
+            Console.WriteLine("Payment result: " + result);
+        }
+    }
+}
---
+++ ProxyDesignPattern/ProxyDesignPattern/ProxyPayment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDesignPattern
+{
+    internal class ProxyPayment : IPayment
+    {
+        private Payment payment;
+        private string paymentApiKey;
+
+        public ProxyPayment(string paymentApiKey)
+        {
+            this.paymentApiKey = paymentApiKey;
+        }
+
+        public bool Pay(decimal amount)
+        {
+            if (string.IsNullOrEmpty(paymentApiKey))
+            {
+                Console.WriteLine("Payment API key is invalid.");
+                return false;
+            }
+
+            if (!IsPaymentApiAvailable())
+            {
+                Console.WriteLine("Could not connect to Payment API.");
+                return false;
+            }
+
+            payment = new Payment();
+            bool result = payment.Pay(amount);
+            if (result)
+            {
+                Console.WriteLine("Payment made successfully.");
+            }
+
+            return result;
+        }
+
+        private bool IsPaymentApiAvailable()
+        {
+            Console.WriteLine("Connecting to Payment API...");
+            return true;
+        }
+    }
+}
